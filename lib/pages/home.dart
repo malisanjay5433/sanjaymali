92ed34172bd6af25553a8b6cf6b8d5d87763a016
Sanjay Mali <sanjay.mali@clark.de>
2023-07-15 15:30:12 +0200

    feat(page): add home

---
+++ lib/pages/home.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:sanjaymali/helper/app.colors.dart';
+import 'package:sanjaymali/helper/constants.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+import '../helper/app.style.dart';
+import '../helper/appconstants.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final ItemScrollController _itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+  final ScrollOffsetListener scrollOffsetListener =
+      ScrollOffsetListener.create();
+  final onMenuHover = Matrix4.identity()..scale(1.0);
+  final menuItems = <String>[
+    'About',
+    'Services',
+    'Portfolio',
+    'Contact',
+  ];
+
+  var menuIndex = 0;
+
+  final screensList = const <Widget>[
+    Home()
+    // AboutMe(),
+    // MyServices(),
+    // MyPortfolio(),
+    // ContactUs(),
+    // FooterClass(),
+  ];
+
+  Future scrollTo({required int index}) async {
+    _itemScrollController
+        .scrollTo(
+            index: index,
+            duration: const Duration(seconds: 2),
+            curve: Curves.fastLinearToSlowEaseIn)
+        .whenComplete(() {
+      setState(() {
+        menuIndex = index;
+      });
+    });
+  }
+
+  final yourScrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: AppColors.appBarColor,
+        toolbarHeight: AppConstant.appBarHeight,
+        titleSpacing: AppConstant.appBarTitleSpace,
+        elevation: 0,
+        title: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth < 768) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Spacer(),
+                  PopupMenuButton(
+                    icon: Icon(
+                      Icons.menu_sharp,
+                      size: 32,
+                      color: AppColors.white,
+                    ),
+                    color: AppColors.bgColor2,
+                    position: PopupMenuPosition.under,
+                    constraints:
+                        BoxConstraints.tightFor(width: size.width * 0.9),
+                    itemBuilder: (BuildContext context) => menuItems
+                        .asMap()
+                        .entries
+                        .map(
+                          (e) => PopupMenuItem(
+                            textStyle: AppTextStyles.headerTextStyle(),
+                            onTap: () {
+                              scrollTo(index: e.key);
+                            },
+                            child: Text(e.value),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ],
+              );
+            } else {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    AppConstant.appName,
+                    style: AppTextStyles.headingAppName(fontSize: 40.0),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: 30,
+                    child: ListView.separated(
+                      itemCount: menuItems.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (context, child) =>
+                          Constants.sizedBox(width: 8),
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            scrollTo(index: index);
+                          },
+                          borderRadius: BorderRadius.circular(100),
+                          onHover: (value) {
+                            setState(() {
+                              if (value) {
+                                menuIndex = index;
+                              } else {
+                                menuIndex = 0;
+                              }
+                            });
+                          },
+                          child: buildNavBarAnimatedContainer(
+                              index, menuIndex == index ? true : false),
+                        );
+                      },
+                    ),
+                  ),
+                  Constants.sizedBox(width: 30),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+      body: Scrollbar(
+        trackVisibility: true,
+        thumbVisibility: true,
+        thickness: 8,
+        interactive: true,
+        controller: yourScrollController,
+        child: ScrollablePositionedList.builder(
+          itemCount: screensList.length,
+          itemScrollController: _itemScrollController,
+          itemPositionsListener: itemPositionsListener,
+          scrollOffsetListener: scrollOffsetListener,
+          itemBuilder: (context, index) {
+            return screensList[index];
+          },
+        ),
+      ),
+    );
+  }
+
+  //TODO: Can change color and radius corner can be added
+
+  AnimatedContainer buildNavBarAnimatedContainer(int index, bool hover) {
+    return AnimatedContainer(
+      alignment: Alignment.center,
+      width: hover ? 80 : 75,
+      duration: const Duration(milliseconds: 200),
+      transform: hover ? onMenuHover : null,
+      child: Text(
+        menuItems[index],
+        style: AppTextStyles.headerTextStyle(
+            color: hover ? AppColors.white : AppColors.white),
+      ),
+    );
+  }
+}
+
+class HomePage {}
